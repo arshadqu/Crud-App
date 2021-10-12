0c8e000b8ca7799d1db7d7f10680b3466de15d0a
arshadqu <91937645+arshadqu@users.noreply.github.com>
2021-10-12 11:55:54 +0530

    Add files via upload

--- src/Components/tutorials.js
+++ src/Components/tutorials.js
@@ -7,18 +7,30 @@ import { Link } from 'react-router-dom';
 import ReadOnlyRow from './ReadOnlyRow';
 const Tutorials = () =>{
     const[courses,setCourses] = useState(data);
-
+    const[searchTerm, setSearchTerm] = useState("");
     return (
         
         <div className="tutorialsList">
             <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Course Name" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                <input 
+                type="text" 
+                className="form-control" 
+                onChange = {(event)=>{
+                    setSearchTerm(event.target.value)
+                }}
+                placeholder="Course Name" aria-label="Recipient's username" aria-describedby="button-addon2"/>
                 <button className="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
             </div>
             <div>
                 <div>
                     <ul className = "list-group">
-                        {courses.map((courses) => (
+                        {data.filter((courses)=>{
+                            if(searchTerm == "") {
+                                return courses
+                            }    else if(courses.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                return courses
+                            }
+                        }).map((courses) => (
                                <ReadOnlyRow courses={courses}/>
                                 ))}
                     </ul>
